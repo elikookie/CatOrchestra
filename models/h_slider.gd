b00b31f69f931f4c80276e056ad5e37db1a5802c
Elitsa Koleva <53860330+elikookie@users.noreply.github.com>
2024-12-01 22:21:13 +0000

    ui for sound controller screen

---
+++ models/h_slider.gd
@@ -0,0 +1,12 @@
+extends HSlider
+
+func _on_value_changed(value):
+	AudioServer.set_bus_volume_db(0,value)
+	
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
