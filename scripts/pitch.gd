f8fe02ff02458e09366c23a55a5bd04ca2bdbb55
Elitsa Koleva <53860330+elikookie@users.noreply.github.com>
2024-12-02 03:58:54 +0000

    added audio buses for every button sound and made sure logic is right for the effects

---
+++ scripts/pitch.gd
@@ -0,0 +1,18 @@
+extends HSlider
+
+func _on_value_changed(value):
+	var bus_index = 1
+	var effect_index = 2
+	var effect = AudioServer.get_bus_effect(bus_index, effect_index)
+	if effect and effect is AudioEffectPitchShift:
+		effect.set_pitch_scale(value)
+	else:
+		print("Effect is not a PitchShift effect or is missing.")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
