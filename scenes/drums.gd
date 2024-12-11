9596c8f23892396a4e9e6263f5ed990401bf33de
Onting <ontingleung@tutamail.com>
2024-12-11 00:48:42 +0000

    colision for tom working if stick is dropped on top of it

--- scenes/drums.gd
+++ scenes/drums.gd
@@ -1,6 +1,5 @@
 extends Node3D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,6 +11,7 @@ func _process(delta: float) -> void:
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	print("AAAAAAAAAAAAAAAAAAAAAA")
-	$AudioStreamPlayer3D.play()
-	$AnimationPlayer.play("floortomHIT")
+	var floortomAudioPlayer = get_node("floortom_001/Area3D/AudioStreamPlayer3D")
+	var animationPlayer = get_node("AnimationPlayer")
+	floortomAudioPlayer.play()
+	animationPlayer.play("floortomHIT")
